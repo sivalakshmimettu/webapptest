88852c2284fa43eb5ed0344cb6b9074f2da68cdb
sivalakshmimettu <sivalakshmi.mettu@valuemomentum.com>
2025-02-07 13:01:59 +0530

    Update main.tf

--- webapp/main.tf
+++ webapp/main.tf
@@ -1,24 +1,58 @@
-# terraform/main.tf
-# Define the resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+# Configure the Azure provider
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.64.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
 }
+
 provider "azurerm" {
   features {}
-  subscription_id ="f936a180-7b93-4203-8faa-f376529bd4f8"
-  client_id       ="a2d89136-b086-4755-9f98-af856c2d8c30"
-  client_secret  = "QEt8Q~el5WQ8JWv56IsRMvM5mSJeMHsflwyEJbQ7"
-  tenant_id      = "13085c86-4bcb-460a-a6f0-b373421c6323"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret  = var.client_secret
+  tenant_id      = var.tenant_id
 }
 
-# Use environment-specific values dynamically
-module "webapp" {
-  source              = "./modules/webapp"
-  web_app_name            = var.web_app_name
-  environment         = var.environment
-  resource_group_name      = var.resource_group_name
-  location            = var.location
-  app_service_plan_name = var.app_service_plan_name
+
+# Define the resource group
+data "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+ }
+
+# Define the Service Plan
+resource "azurerm_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  # Define SKU attributes directly
+  sku_name            = var.sku_size  # This should be the full SKU name, like "B1", "S1", etc.
+  os_type             = var.os_type  # "Linux" or "Windows"
 }
 
+resource "random_string" "unique" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+
+# Define the Web App
+resource "azurerm_app_service" "example" {
+  name                = "${var.web_app_name}-${random_string.unique.result}"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_service_plan.example.id
+
+  app_settings = {
+    "WEBSITE_NODE_DEFAULT_VERSION" = "14"
+  }
+}
